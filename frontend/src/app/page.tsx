f8a9e028d2abea44cc4207b09275fe26a79fb099
HolyShaq <acebyronehalili@gmail.com>
2025-01-08 16:46:56 +0800

    fix: only instantiate websocket once on mount

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import Image from "next/image";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
-  const ws = new WebSocket("ws://localhost:8000/api/v1/ws/global/");
+  const ws = useRef<WebSocket>(null)
 
-  ws.onmessage = (event) => {
-    console.log(JSON.parse(event.data));
-  };
+  useEffect(() => {
+    ws.current = new WebSocket("ws://localhost:8000/api/v1/ws/global/");
+
+    ws.current.onmessage = (event) => {
+      console.log(JSON.parse(event.data));
+    };
+  }, [])
 
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center bg-black">
@@ -17,8 +22,8 @@ export default function Home() {
         width={100}
         height={24}
         priority
-        onClick={() => ws.send(JSON.stringify({
-          user_id: 123,
+        onClick={() => ws.current!.send(JSON.stringify({
+          user_id: 1,
           text: "Test message",
           timestamp: new Date()
         }))}
