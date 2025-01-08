2658a761470fd93a9f53777141a33c126c840bfc
HolyShaq <acebyronehalili@gmail.com>
2025-01-08 17:48:35 +0800

    fix: dynamic import of theme provider to remove hydration warning

--- frontend/src/components/theme-provider.tsx
+++ frontend/src/components/theme-provider.tsx
@@ -1,11 +1,17 @@
-"use client"
+'use client'
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import * as React from 'react'
+const NextThemesProvider = dynamic(
+	() => import('next-themes').then((e) => e.ThemeProvider),
+	{
+		ssr: false,
+	}
+)
 
-export function ThemeProvider({
-  children,
-  ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
+
+import dynamic from 'next/dynamic'
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+	return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
