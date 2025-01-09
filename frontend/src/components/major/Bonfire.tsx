0c4f7577fe939f64b997af6b330db86a2659ead8
HolyShaq <acebyronehalili@gmail.com>
2025-01-09 15:22:17 +0800

    feat: added scaffolding for bonfire page

--- frontend/src/components/major/Bonfire.tsx
+++ frontend/src/components/major/Bonfire.tsx
@@ -1,3 +1,17 @@
+"use client";
+
+import { useState } from "react";
+import ChatInput from "../ChatInput";
+import { Input } from "../ui/input";
+import { PaperAirplaneIcon } from "@heroicons/react/16/solid";
+
 export default function Bonfire() {
-  return <div>Bonfire</div>;
+  const [chatInput, setChatInput] = useState("");
+
+  return (
+    <div className="ml-2 mr-4 mb-10 flex h-screen flex-grow flex-col">
+      <div className="flex flex-grow flex-col-reverse items-end justify-start px-4"></div>
+      <ChatInput setChatInput={setChatInput} onSend={() => {}} />
+    </div>
+  );
 }
