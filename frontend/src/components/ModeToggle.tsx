964d4402fed75db73d92a75cdff88ef55d5914a9
HolyShaq <acebyronehalili@gmail.com>
2025-01-09 16:12:44 +0800

    chore: make dark mode toggle icons solid

--- frontend/src/components/ModeToggle.tsx
+++ frontend/src/components/ModeToggle.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Moon, Sun } from "lucide-react";
+import { MoonIcon, SunIcon } from "@heroicons/react/16/solid";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
@@ -17,8 +17,8 @@ export function ModeToggle() {
         setTheme(theme == "light" ? "dark" : "light");
       }}
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 text-primary-foreground dark:text-primary transition-all group-hover:motion-preset-pulse-md dark:-rotate-90 dark:scale-0 dark:group-hover:text-primary-foreground" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 text-primary-foreground dark:text-primary transition-all group-hover:motion-preset-pulse-md dark:rotate-0 dark:scale-100 group-hover:text-primary-foreground" />
+      <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 text-primary-foreground dark:text-primary transition-all group-hover:motion-preset-pulse-md dark:-rotate-90 dark:scale-0 dark:group-hover:text-primary-foreground" />
+      <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 text-primary-foreground dark:text-primary transition-all group-hover:motion-preset-pulse-md dark:rotate-0 dark:scale-100 group-hover:text-primary-foreground" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
