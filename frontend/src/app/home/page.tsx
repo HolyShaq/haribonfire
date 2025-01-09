66f4d61327c3ff75c6f9ba069888e52f74a3d365
HolyShaq <acebyronehalili@gmail.com>
2025-01-09 15:43:14 +0800

    feat: added utility function to generate avatars

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -9,6 +9,7 @@ import PersonCard from "@/components/PersonCard";
 import RandomSVG from "@/components/RandomSVG";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { avatar } from "@/lib/utils";
 import { Cog6ToothIcon } from "@heroicons/react/16/solid";
 import { useState } from "react";
 
@@ -98,7 +99,7 @@ function Sidebar({ setSelectedContent }: Readonly<SidebarProps>) {
             <PersonCard
               key={i}
               name={`Person ${i}`}
-              avatar={`https://robohash.org/${i}?set=set4`}
+              avatar={avatar(i)}
               course="Computer Science"
               setSelectedContent={setSelectedContent}
               i={i}
@@ -109,7 +110,7 @@ function Sidebar({ setSelectedContent }: Readonly<SidebarProps>) {
       <div className="flex w-full items-center justify-between rounded-md bg-stone-200 pr-2 dark:bg-stone-900">
         <div className="flex w-full items-center space-x-2 px-2 rounded-md transition-all hover:bg-stone-300 dark:hover:bg-stone-800">
           <Avatar className="m-1 ml-0 h-12 w-12">
-            <AvatarImage src={"https://robohash.org/acehalili?set=set4"} />
+            <AvatarImage src={avatar("Ace Byrone")} />
             <AvatarFallback>Ac</AvatarFallback>
           </Avatar>
           <div className="flex flex-col items-start justify-center">
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function avatar(value: number | string, set: number = 4) {
+  return `https://robohash.org/${value}?set=set${set}`
+}
