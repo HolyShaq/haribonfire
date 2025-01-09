66fd36e4bd13c1d5995b7fcae14976411656e8d7
HolyShaq <acebyronehalili@gmail.com>
2025-01-09 18:58:50 +0800

    feat: added utility function generate random names

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { uniqueNamesGenerator, adjectives, animals } from "unique-names-generator"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -7,4 +8,14 @@ export function cn(...inputs: ClassValue[]) {
 
 export function avatar(value: number | string, set: number = 4) {
   return `https://robohash.org/${value}?set=set${set}`
+}
+
+export function randomName() {
+  const name = uniqueNamesGenerator({
+    dictionaries: [adjectives, animals],
+    separator: " ",
+    length: 2,
+    style: "capital",
+  })
+  return name
 }
