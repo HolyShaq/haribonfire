1d061034385c476d81714f3c7500a4a31469658e
HolyShaq <acebyronehalili@gmail.com>
2025-01-09 16:41:25 +0800

    feat: added message component

---
+++ frontend/src/components/Message.tsx
@@ -0,0 +1,44 @@
+import { avatar, cn } from "@/lib/utils";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+interface MessageProps {
+  user_id: number;
+  text: string;
+  timestamp: string;
+  show_sender?: boolean;
+}
+
+// TODO: Replace with dynamic data
+export default function Message({
+  user_id,
+  text,
+  timestamp,
+  show_sender = true,
+}: MessageProps) {
+  return (
+    <div className={ cn( "flex flex-row items-center justify-start space-x-3 mb-6", !show_sender && "mb-[14px] mt-[-26px]" ) }>
+      {show_sender ? (
+        <Avatar className="h-10 w-10">
+          <AvatarImage src={avatar(user_id)} />
+          <AvatarFallback>Ac</AvatarFallback>
+        </Avatar>
+      ) : (
+        <div className="h-10 w-10" />
+      )}
+
+      <div className="flex flex-col items-start justify-end space-y-2">
+        {show_sender && (
+          <div className="flex flex-row items-end justify-end space-x-2">
+            <span className="font-semibold leading-none">
+              Ace Byrone Halili
+            </span>
+            <span className="text-xs leading-none text-muted-foreground">
+              {new Date(timestamp).toLocaleString()}
+            </span>
+          </div>
+        )}
+        <span className={ cn( "text-sm leading-none font-light" ) }>{text}</span>
+      </div>
+    </div>
+  );
+}
