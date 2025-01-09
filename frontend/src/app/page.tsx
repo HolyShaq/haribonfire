559ebdbd8d03a29b32087ae10acfacc30b588fbc
HolyShaq <acebyronehalili@gmail.com>
2025-01-09 10:24:19 +0800

    feat: changed base route to draft splash screen

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,35 +1,18 @@
 "use client";
 
-import { ModeToggle } from "@/components/ModeToggle";
-import Image from "next/image";
-import { useEffect, useRef } from "react";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  const ws = useRef<WebSocket>(null)
-
-  useEffect(() => {
-    ws.current = new WebSocket("ws://localhost:8000/api/v1/ws/global/");
-
-    ws.current.onmessage = (event) => {
-      console.log(JSON.parse(event.data));
-    };
-  }, [])
-
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center bg-background">
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        width={100}
-        height={24}
-        priority
-        onClick={() => ws.current!.send(JSON.stringify({
-          user_id: 1,
-          text: "Test message",
-          timestamp: new Date()
-        }))}
-      />
-      <ModeToggle />
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-background">
+      <span className="text-4xl font-bold">Welcome to Haribonfire!</span>
+      <Button className="mt-4" variant="outline"
+        onClick={() => {
+          window.location.href = "/home/"
+        }}
+      >
+        Log in with Microsoft
+      </Button>
     </div>
   );
 }
