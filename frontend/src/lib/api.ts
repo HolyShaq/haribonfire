da35ca4559f9d1ffcd996f515408bfa414f4d449
HolyShaq <acebyronehalili@gmail.com>
2025-01-11 21:34:51 +0800

    fix: convert timezone properly

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,33 +1,38 @@
-import { Message } from '@/common/interfaces';
-import axios from 'axios';
+import { Message } from "@/common/interfaces";
+import axios from "axios";
 
-const API_BASE_URL = 'localhost:8000/api/v1/'
-const HTTP_BASE_URL = `http://${API_BASE_URL}`
-const WEBSOCKET_BASE_URL = `ws://${API_BASE_URL}`
+const API_BASE_URL = "localhost:8000/api/v1/";
+const HTTP_BASE_URL = `http://${API_BASE_URL}`;
+const WEBSOCKET_BASE_URL = `ws://${API_BASE_URL}`;
 
 // RESTs
 export async function getGlobalMessages() {
   try {
-    const response = await axios.get(HTTP_BASE_URL + 'messages/global/')
-    return response.data
+    const response = await axios.get(HTTP_BASE_URL + "messages/global/");
+    return response.data.map((message: Message) => ({
+      ...message,
+      sent_at: new Date(message.sent_at + "Z").toISOString(), // Convert to local timezone
+    }));
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
 // Websockets
-export function getGlobalWebsocket(setMessages: React.Dispatch<React.SetStateAction<Message[]>>) {
-  const ws = new WebSocket(WEBSOCKET_BASE_URL + 'ws/global/')
+export function getGlobalWebsocket(
+  setMessages: React.Dispatch<React.SetStateAction<Message[]>>,
+) {
+  const ws = new WebSocket(WEBSOCKET_BASE_URL + "ws/global/");
   ws.onerror = (event) => {
-    console.log(event)
-  }
+    console.log(event);
+  };
   ws.onopen = () => {
-    console.log('Connected to global websocket')
-  }
+    console.log("Connected to global websocket");
+  };
   ws.onmessage = (event) => {
-    const data = JSON.parse(event.data)
-    console.log(data, typeof data)
-    setMessages((prev) => [...prev, data])
-  }
-  return ws
+    const data = JSON.parse(event.data);
+    console.log(data, typeof data);
+    setMessages((prev) => [...prev, data]);
+  };
+  return ws;
 }
