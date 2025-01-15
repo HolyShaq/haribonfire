450d94c42ba5ccbaf485b3e014a45dea51adf710
HolyShaq <acebyronehalili@gmail.com>
2025-01-15 10:48:58 +0800

    feat: added basic random chat frontend

--- frontend/src/components/major/Random.tsx
+++ frontend/src/components/major/Random.tsx
@@ -1,3 +1,26 @@
-export default function Bonfire() {
-  return <div>Random</div>;
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+
+export default function Random() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div>
+        <span className="text-3xl font-bold">Match with a </span>
+        <span className="text-3xl font-bold text-primary">random</span>
+        <span className="text-3xl font-bold"> Haribon!</span>
+      </div>
+
+      <div className="flex flex-row mt-6 items-center space-x-2">
+        <Checkbox id="prefcourse" />
+        <label htmlFor="prefcourse">Prefer same course</label>
+      </div>
+
+      <Button
+        className="mt-3 bg-primary text-primary-foreground"
+        variant="outline"
+      >
+        Let's go!
+      </Button>
+    </div>
+  );
 }
