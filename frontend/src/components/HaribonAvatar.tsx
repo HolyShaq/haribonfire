05f5cdc56f38cd4fa6a3d845a30e881446151c43
HolyShaq <acebyronehalili@gmail.com>
2025-04-23 18:50:43 +0800

    feat: profile customization modal

--- frontend/src/components/HaribonAvatar.tsx
+++ frontend/src/components/HaribonAvatar.tsx
@@ -1,4 +1,4 @@
-import { avatar } from "@/lib/utils";
+import { avatar, cn } from "@/lib/utils";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { User } from "@/common/interfaces";
 
@@ -6,15 +6,17 @@ interface HaribonAvatarProps {
   user?: User;
   name?: string;
   avatar_seed?: string;
+  avatar_style?: string;
 }
 export default function HaribonAvatar({
   user,
   name = "",
   avatar_seed = "",
+  avatar_style = "",
 }: HaribonAvatarProps) {
   // If user does not exist, use the optional parameters instead
   return (
-    <Avatar className="h-10 w-10">
+    <Avatar className={cn("h-10 w-10", avatar_style)}>
       <AvatarImage src={avatar(user ? user.avatar_seed : avatar_seed)} />
       <AvatarFallback>
         {user ? user.name.slice(0, 2) : name.slice(0, 2)}
--- frontend/src/components/UserSettings.tsx
+++ frontend/src/components/UserSettings.tsx
@@ -1,17 +1,14 @@
 import { User } from "@/common/interfaces";
-import { avatar } from "@/lib/utils";
-import { ModeToggle } from "./ModeToggle";
-import { Cog6ToothIcon } from "@heroicons/react/16/solid";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ArrowPathIcon } from "@heroicons/react/16/solid";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
-  DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
 import HaribonAvatar from "./HaribonAvatar";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 interface UserSettingsProps {
   user: User;
@@ -30,9 +27,29 @@ export default function UserSettings({ user }: UserSettingsProps) {
   );
 
   const DialogSection = () => (
-    <div className="flex">
-      <div className="relative">
+    <div className="flex flex-col px-3 space-y-6 w-full items-center">
+      <div className="flex space-x-6 w-full">
+        <div className="flex flex-col w-full space-y-3">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-bold pl-3">Display Name</p>
+            <Input defaultValue={user.name} className="w-full" />
+          </div>
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-bold pl-3">Course</p>
+            <Input defaultValue={"Computer Science"} className="w-full" />
+          </div>
+        </div>
+        <div className="relative mt-3">
+          <HaribonAvatar
+            user={user}
+            avatar_style="h-32 w-32 outline outline-stone-700"
+          />
+          <ArrowPathIcon className="h-10 w-10 absolute bottom-1 right-1 text-primary"/>
+        </div>
       </div>
+      <Button>
+        Save Changes
+      </Button>
     </div>
   );
 
@@ -41,10 +58,9 @@ export default function UserSettings({ user }: UserSettingsProps) {
       <DialogTrigger>
         <UserSection />
       </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Profile Settings</DialogTitle>
-        </DialogHeader>
+      <DialogContent className="w-[500px]">
+        <DialogTitle />
+        <DialogSection />
       </DialogContent>
     </Dialog>
   );
