1ba0231fc344337a86ecc38fdc599d96815f87a7
HolyShaq <acebyronehalili@gmail.com>
2025-08-02 00:46:28 +0800

    fix: fix bruh moment env loading

--- backend/endpoints/auth.py
+++ backend/endpoints/auth.py
@@ -18,7 +18,7 @@ from database.controllers.users import create_user
 load_dotenv("./.env")
 STATE = "12345"
 NONCE = "6789"
-FRONTEND_URL = load_dotenv("FRONTEND_URL")
+FRONTEND_URL = os.getenv("FRONTEND_URL")
 
 
 async def get_jwks():
