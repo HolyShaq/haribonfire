6c5c98e87d70fe9e51f8ae8a849605a7ce470a69
HolyShaq <acebyronehalili@gmail.com>
2025-08-03 21:41:44 +0800

    fix: add dynamic CORS allow

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -19,9 +21,16 @@ app = FastAPI(lifespan=lifespan)
 
 
 # Allow everything through CORS during dev
+load_dotenv("./.env")
+FRONTEND_URL = os.getenv("FRONTEND_URL")
+FRONTEND_URL = (
+    FRONTEND_URL
+    if FRONTEND_URL and os.getenv("ENVIRONMENT") == "production" 
+    else "http://localhost:3000"
+)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
